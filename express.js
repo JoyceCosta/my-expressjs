6ded624372ed33ecc19ab6a3aadc74c6b8899a7b
Joyce Costa <joycecosta90@gmail.com>
2020-12-06 22:55:31 -0300

    creating an express.js app with middleware functions and returning response

--- express.js
+++ express.js
@@ -1,1 +1,25 @@
 const express = require ('express');
+
+const app = express();
+
+app.use('/', (req, resp, next) => {
+  console.log('Covid');
+  next();
+})
+
+app.use('/symptom-1', (req, resp, next) => {
+  console.log('first');
+  resp.send('<h1>No Smell</h1>');
+});
+
+app.use("/symptom-2", (req, resp, next) => {
+	console.log("first");
+	resp.send("<h1>No Taste</h1>");
+});
+
+app.use((req, resp, next) => {
+  console.log('whatever "/" page');
+  resp.send('<h1>I hate covid</h1>');
+});
+
+app.listen(3000);
